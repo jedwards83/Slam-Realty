413bc78c6804532fce963039ddc1c2c698ad7f8f
Jonathan Edwards <Jonathan.edwardsctc@gmail.com>
2020-07-29 16:07:08 -0400

    Added ReadMe Corrected Forms Next editing Tabs.

---
+++ README.txt
@@ -0,0 +1,8 @@
+SLAM REALTY PROJECT
+-------------------
+
+The Purpose of this project is to be an easy access method to input your contact information and your interest in either Buying, Selling, Renting a house. There is also an option for investors who want to support the company.
+
+Each Image has a button embedded into it that will populate a form. That form is there to transmit data back through php to a database(This will be implemented after Code Louisville Completion). Currently We just have the functionality of the form without the storing of any data.
+
+Each Button in the top right will link to a page that will have the form from the main index page unhidden with Lorem Ipsum on each page which would outline why people would Buy, Rent, Sell, or Invest in Slam Realty.
--- realty.js
+++ realty.js
@@ -1,7 +1,39 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+function openForm1() {
+    document.getElementById("openSellForm").style.display = "block";
+    document.getElementById("closeSellForm").style.display = "none";
   }
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+  function closeForm1() {
+    document.getElementById("openSellForm").style.display = "none";
+    document.getElementById("closeSellForm").style.display = "block";
+  }
+
+  function openForm2() {
+    document.getElementById("openBuyForm").style.display = "block";
+    document.getElementById("closeBuyForm").style.display = "none";
+  }
+  
+  function closeForm2() {
+    document.getElementById("openBuyForm").style.display = "none";
+    document.getElementById("closeBuyForm").style.display = "block";
+  }
+
+  function openForm3() {
+    document.getElementById("openRentalForm").style.display = "block";
+    document.getElementById("closeRentalForm").style.display = "none";
+  }
+  
+  function closeForm3() {
+    document.getElementById("openRentalForm").style.display = "none";
+    document.getElementById("closeRentalForm").style.display = "block";
+  }
+
+  function openForm4() {
+    document.getElementById("openInvestorForm").style.display = "block";
+    document.getElementById("closeInvestorForm").style.display = "none";
+  }
+  
+  function closeForm4() {
+    document.getElementById("openInvestorForm").style.display = "none";
+    document.getElementById("closeInvestorForm").style.display = "block";
   }
